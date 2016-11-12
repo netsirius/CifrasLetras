027b87f2b8bdb36b6fcefdc590fcec2d78557161
netsirius <netsirius@netsirius-pc>
2016-11-12 13:21:58 +0100

    generacion Cifras aleatorias

--- src/cifrasletras/CifrasLetras.java
+++ src/cifrasletras/CifrasLetras.java
@@ -5,17 +5,38 @@
  */
 package cifrasletras;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author netsirius
  */
 public class CifrasLetras {
-
+    private static final Random r = new Random();
     /**
-     * @param args the command line arguments
+     * A partir de un numero n, n numeros del rango (1-10, 35, 75 i 100).
+     * @param n
+     * @return array con las cifras generadas
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public int[] generarCifras(int n){
+        int cifras[] = new int[n*5];
+        int rangos[] = {10,25,50,75,100};
+        for (int i = 0; i < cifras.length; i++) {
+            for (int j = 0; j < rangos.length; j++) {
+                cifras[i] = r.nextInt(rangos[j])+1; 
+            }
+        }
+        Arrays.sort(cifras);
+        return cifras;
     }
-    
+    /**
+     * Generamos numero k, es el número al cual hay que aproximarse o obtener a aprtir de
+     * las operaciones elementales (+, -, ×, ÷) realizadas con las cifras generadas.
+     * @return numero esperado k
+     */
+    public int numk(){
+        int k = r.nextInt(10000)+101;
+        return k;
+    }
 }
